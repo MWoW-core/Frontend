4f44057bf4ef0d26eb1ceb3fce2fd644d512af02
sasin91 <jonas.kerwin.hansen@gmail.com>
2020-01-19 18:10:23 +0100

    Added download  realmlist link

--- components/NavbarLeft.vue
+++ components/NavbarLeft.vue
@@ -3,10 +3,28 @@
     <portal-target name="navbar" slim />
 
     <a
-      class="text-brand text-shadow"
+      :href="downloadRealmlistURL"
+      target="_blank"
+      class="text-brand text-shadow border-r border-l border-brand px-2"
+    >
+      Download Realmlist
+    </a>
+
+    <a
+      class="text-brand text-shadow px-2"
       href="magnet:?xt=urn:btih:b296ea8947b36c68f6e022f5a642ecc406ad8968&dn=World%20of%20Warcraft%203.3.5a%20%28no%20install%29"
     >
       Download 3.3.5a
     </a>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    downloadRealmlistURL() {
+      return `${process.env.API_URL}/download-realmlist`
+    }
+  }
+}
+</script>
